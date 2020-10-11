26fde55defc898e0e2a497063d5a39595a21e020
TaviaC91 <TaviaC91@gmail.com>
2020-10-11 12:41:47 -0400

    layout instructions for quiz

---
+++ js/apps.js
@@ -0,0 +1,40 @@
+//User lands on the website, the game pops up on the screen//
+
+
+//Storage  for user input//
+
+
+
+//Function for the score button//
+
+
+
+//User hits start button// //Tool// 
+
+
+
+//User hits next button to go forth to the next question // No back button//
+
+
+
+//The questions appear, Question 1, Questioin 2, Question 3, Question 4 choose one answer, a,b,c,d//
+
+
+
+// Timer for the question & answers 3 seconds, Timer for next question // Tool//
+
+
+
+//If the answer is incorrect- the answer turns "RED"//
+
+
+
+// If the answer is correct- the answer turns "GREEN"//
+
+
+
+//If the answer to the question is correct, animation flower grows//
+
+
+
+// End game// or //Restart//
