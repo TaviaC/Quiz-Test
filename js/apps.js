9cbabaade3004d590207e351e13d63320de88ee8
TaviaC91 <TaviaC91@gmail.com>
2020-10-13 08:27:20 -0400

    updated functions

--- js/apps.js
+++ js/apps.js
@@ -1,78 +1,13 @@
 //User lands on the website, the game pops up on the screen//
-
-
 //Storage  for user input//
-
-
-
 //Function for the score button//
-
-
-
 //User hits start button// //Tool// 
-
-
-
 //User hits next button to go forth to the next question // No back button//
-
-
-
 //The questions appear, Question 1, Questioin 2, Question 3, Question 4 choose one answer, a,b,c,d//
-
-
-
 // Timer for the question & answers 3 seconds, Timer for next question // Tool//
-
-
-
 //If the answer is incorrect- the answer turns "RED"//
-
-
-
 // If the answer is correct- the answer turns "GREEN"//
-
-
-
 //If the answer to the question is correct, animation flower grows//
-
-
-
 // End game// or //Restart//
-
-
-function enterName () {
-//User enters their name for User input
-console.log('enterName');
-};
-
-
-function scoreChart () {
-//Score chart to keep track of the scores
-console.log('scoreChart');
-}
-
-function questions () {
-//Question 1 populates on the screen
-console.log('question1');
-};
-
-function answers () {
-console.log('answers');
-//Answer populates on the screen, user chooses one, A,B,C,D
-
-
-
-
-
-
-
-};
-
-
-
-
-
-
-
 
 
