feb8e46c20fc72bd2d73791995cbb13d0c8c1a7e
TaviaC91 <TaviaC91@gmail.com>
2020-10-15 22:13:45 -0400

    changed photo in js

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # Quiz-Test
 
 Welcome to Quiz Challenge.
-As the User, you will log onto the site and ender your name
 
-Welcome to the Quiz"____________"
+As the User, you will log onto the site and enter your name
+
+Welcome to the Quiz"______(enter your name)______"
 
 Please choose the correct answer to Question 1
-depending on your response, you see a picture populate with the correct answer and the incorrect answer
 
-There will be a score board that keeps track of your answers
+depending on your response, you see a picture populate with the "correct" answer or the "incorrect" answer
 
-Thanks for playing
+There will be a score section at the top of the page that keeps track of your answers
+
+Thanks for playing!
--- js/apps.js
+++ js/apps.js
@@ -1,5 +1,6 @@
 $(() => {
-    
+    //User storage
+    //w3schools.com
     var person = prompt('Please enter your name', 'Anonymous');
     if (person != null) {
         localStorage.setItem('playerName', person);
@@ -22,16 +23,15 @@ $(() => {
 
         if (type === 1) {
 
-//Select <div id="resultsImage"></div> by ID with Jquery and add this HTML inside of the element 
 
-            $('#resultsImage').html('<h3 class="correct">CORRECT!</h3><img src="https://s.yimg.com/ny/api/res/1.2/qkKXZXbS52Ka.bx2MW2ocQ--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9NzY4O2g9NDk5/http://media.zenfs.com/en_us/News/afp.com/f2b9090c0e41e23f55251a1adbda42b1c6cf8aa5.jpg">');
+            $('#resultsImage').html('<h3 class="correct">CORRECT!</h3><img src="https://admin.promonews.tv/sites/default/files/styles/bmvd_content_page/public/drake%20laugh%20now.jpg?itok=Nal_X8dG">');
        
         } else {
             $('#resultsImage').html('<h3 class= "incorrect">WRONG ANSWER TRY AGAIN</h3><img src="https://www.gamegrin.com/assets/Uploads/_resampled/croppedimage640200-kanyemad.jpg">');
         }
 
 
-        //Select <div id="results"></div> by ID with Jquery and add this HTML inside of the element AND then SHOW it 
+//Select <div id="results"></div> by ID with Jquery and add this HTML inside of the element AND then SHOW it 
         $('#results').html('<p>Score: ' + answeredCorrectCount + '</p>').show();
     });
 });
